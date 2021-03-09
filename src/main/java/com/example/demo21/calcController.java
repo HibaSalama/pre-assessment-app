d4c7340e6e59325ab5aa62ae70d2fb10e74a6e7a
Salama <Hiba.Salama1@T-Mobile.com>
2021-03-08 20:29:23 -0800

    added new endpoint

--- src/main/java/com/example/demo21/calcController.java
+++ src/main/java/com/example/demo21/calcController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class calcController {
 
     @GetMapping("www.myapp.com/{num}") 
-
     public static String addTen(@PathVariable int num) {
         int sum = num + 10;
+        return "sum: " + sum;
+    }
+
+    @GetMapping("www.myapp.com/{num}/{num1}") 
+    public static String twoParamAdder(@PathVariable int num, @PathVariable int num1) {
+        int sum = num + num1;
         return "sum: " + sum;
     }
 }
